df23c1aca26a810e4ff54741712c47667a977d78
jayasingh2007 <jayassingh2007@gmail.com>
2025-07-24 17:22:24 +0530

    Add files via upload

---
+++ practical3...py
@@ -0,0 +1,14 @@
+Python 3.11.9 (tags/v3.11.9:de54cf5, Apr  2 2024, 10:12:12) [MSC v.1938 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================================= RESTART: D:/FYCS PYTHON PRACTICAL/PRACTICAL1.py ================================
+hello world
+>>> 
+================================= RESTART: D:/FYCS PYTHON PRACTICAL/practical2.py ================================
+40
+10
+375
+1.6666666666666667
+10
+1
+931322574615478515625
