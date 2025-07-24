8c10ce714e0a39f3fe580f154846916ae299469f
jayasingh2007 <jayassingh2007@gmail.com>
2025-07-24 17:23:04 +0530

    Add files via upload

---
+++ practical4.py
@@ -0,0 +1,9 @@
+a=int(input("enter the first"))
+b=int(input("enter the second"))
+c=int(input("enter the third"))
+if a<b and a<c:
+    print("a is smaller")
+elif b<a and b<c:
+    print("b is smaller")
+else:
+    print("c is smaller")
