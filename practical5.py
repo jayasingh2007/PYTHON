2684c133c8660ae549cde1031db5fa9212bb5bd2
jayasingh2007 <jayassingh2007@gmail.com>
2025-07-24 17:24:00 +0530

    Add files via upload

---
+++ practical5.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    if i==5:
+        continue
+    print(i)
+
+for i in range(5):
+    if i==2:
+        pass
+    print(i)
+
+
+
+for i in range (10):
+    if i==5:
+        break
+    print(i)
