8c060408967503f5022d75bf3512c1ba486edc2c
jayasingh2007 <jayassingh2007@gmail.com>
2025-07-24 17:22:09 +0530

    Add files via upload

---
+++ practical2.py
@@ -0,0 +1,9 @@
+a=25
+b=15
+print(a+b)   #addition
+print(a-b)   #subtraction
+print(a*b)   #multiplication
+print(a/b)   #division
+print(a%b)   #modulus
+print(a//b)  #floor division
+print(a**b)  #exponentiation
